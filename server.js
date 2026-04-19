a23a35e71bf9fb7f85a23458cbabd24aabcba338
isasod2500 <isso2500@student.miun.se>
2026-04-19 18:11:54 +0200

    Configure CORS for specific frontend origin

--- server.js
+++ server.js
@@ -13,6 +13,12 @@ app.use(cors());
 app.use(bodyParser.json())
 
 
+app.use(cors({
+    origin: 'https://lab2frontend.netlify.app/',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+});
+
 
 const port = process.env.PORT || 3000;
 
